b36c56cfdd41509f5b00da72ee59fafceccb77ce
David <davidjsollinger@gmail.com>
2015-11-01 00:09:28 +0000

    changed tabs to spaces

--- main.c
+++ main.c
@@ -8,30 +8,30 @@ int main (int argc, char *argv[]) {
   int c;
   int recurs = 0;
   int writeSize = 0;
-	while ((c = getopt (argc, argv, "rfs:")) != -1) {
-		switch (c) {
-			case 's':
-				writeSize=strtol(optarg, NULL, 10);
-				break;
-			case 'f':
-				break;
-			case 'r':
-				recurs=1;
-				break;
-			default:
-				abort();
-		}
-	}
-	
-	if (optind > argc - 1) {
-		return 0;
-	}
-	
-	switch(recurs) {
-		case 1:
-			deleteFiles(argv[optind]);
-			break;
-		default:
-			deleteFile(argv[optind]);
-	}
+  while ((c = getopt (argc, argv, "rfs:")) != -1) {
+    switch (c) {
+      case 's':
+        writeSize=strtol(optarg, NULL, 10);
+        break;
+      case 'f':
+        break;
+      case 'r':
+        recurs=1;
+        break;
+      default:
+        abort();
+    }
+  }
+  
+  if (optind > argc - 1) {
+    return 0;
+  }
+
+  switch(recurs) {
+    case 1:
+      deleteFiles(argv[optind]);
+      break;
+    default:
+      deleteFile(argv[optind]);
+  }
 }
